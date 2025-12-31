9e3c83137155e5a7723560a7b07bae24373f6a30
alitariq2072008-netizen <alitariq2072008@gmail.com>
2025-12-31 22:22:46 +0300

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,83 @@
+let scoreMonkey=10;
+let scoreSarrow=25;
+scoreMonkey+=5;
+scoreSarrow+=3;
+scoreSarrow*=2;
+scoreMonkey*=2;
+console.log(scoreMonkey);
+console.log(scoreSarrow);
+console.log(scoreSarrow>scoreMonkey);
+console.log(scoreSarrow>=scoreMonkey);
+console.log(scoreMonkey<=scoreSarrow);
+
+let scoreSnake=15;
+
+console.log(scoreSnake>scoreSarrow&&scoreMonkey);
+console.log(scoreSnake>scoreSarrow||scoreMonkey);
+
+console.log("task3 Done");
+ 
+
+
+
+console.log("task4");
+
+let result=10+5*2>20-3;
+console.log(result);
+const a=5;
+const b='5';
+let sum=a+b;
+console.log(sum);
+console.log("task4 done");
+console.log("task5");
+let orginalNumber="5";
+console.log(orginalNumber);
+let convertedNumber=orginalNumber
+orginalNumber+=10;
+console.log(orginalNumber);
+
+
+
+
+
+
+
+
+
+let numberToCheck=true;
+
+console.log(numberToCheck);
+ 
+let before ="20";
+console.log(before);
+let after=before
+before=(5);
+console.log(before);
+
+console.log("task5 done");
+
+
+console.log("task6");
+
+
+let playerName=Christiano;
+let playerScore=950;
+let team=Al-Nassr;
+let championsLeagueWon=4;
+let playerAge=40;
+let templateLiterals = `He  is name ${playerName} He is score ${playerScore} his football team name ${team} They are champions number ${championsLeagueWon} He is year old ${playerAge} `
+console.log(templateLiterals);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
